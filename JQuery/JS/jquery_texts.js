0144faeb4adf7cd75507584df14e898e714a58a1
Pablo Fonseca <pablo.fonsecam@outlook.com>
2020-01-18 21:51:54 -0600

    JQuery Files Added Tutorial 3>Texto Finished
    Finished Completely.

--- JQuery/JS/jquery_texts.js
+++ JQuery/JS/jquery_texts.js
@@ -1,12 +1,34 @@
 'use strict';
+$(document).ready(function(){
 
-$(document).ready(() => {
     console.log("DOM Loaded");
-    let anchors = $("a");
-    $("a").each((indexInArray, valueOfElement) => {
-        var link = $(valueOfElement).attr("href");
-        console.log(link);
-        //debugger;
-        $(valueOfElement).text(link);
+    reloadLinks();
+    let add_client_link = $("#add_client_link");
+    let submit_data = $("#submit_data");
+
+  add_client_link.keyup(function(){
+      if($(this).val().length >= 1){
+        submit_data.removeAttr("disabled");
+      }else{
+        submit_data.attr("disabled", "disabled");
+      }
     });
+    submit_data.click(function(){
+      //let link_list = $("#link_list");
+      //Reset input value
+      $("#link_list").append('<li><a href="' + $("#add_client_link").val() + '"></a></li>');
+      reloadLinks();
+      add_client_link.val("");
+    });
+
+    function reloadLinks(){
+      $('a').each(function(indexInArray){
+        let eachElement = $(this);
+        let link = eachElement.attr("href");
+        eachElement.attr("target", "_blank");
+
+        console.log(link);
+        $(eachElement).text(link);
+      });
+    }
 });
